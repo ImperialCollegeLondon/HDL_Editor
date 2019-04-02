e9d962adc5854b0b1a6ecce4b53b8b9e8f5da95a
wilsonwang881 <mitfans881@gmail.com>
2019-04-02 13:57:43 +0100

    start testing js libraries. Nothing useful at this stage

---
+++ js/button_click.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  
+  function updateButton() {
+    // var counter = 1;
+    console.log("button clicked!");
+    // button.innerHTML = counter;
+    // counter += 1;
+  }
+
+  document.getElementById('elmish-button').addEventListener('click', updateButton);
+
+}
---
+++ js/jointjs-add-shape.js
@@ -0,0 +1,15 @@
+nodeModule.exports.drawRectangle = function(positionX, positionY) {
+  var rect = new joint.shapes.standard.Rectangle();
+  rect.position(positionX, positionY);
+  rect.resize(100, 40);
+  rect.attr({
+      body: {
+          fill: 'blue'
+      },
+      label: {
+          text: 'Hello',
+          fill: 'white'
+      }
+  });
+  return rect;
+}
