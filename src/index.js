047e1245176a8c9b6a2d1bf9ac62f4c73a33cbfa
wilsonwang881 <mitfans881@gmail.com>
2019-04-07 00:03:09 +0100

    testing asset management on webpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // add the image to the existing div
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,30 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 };
