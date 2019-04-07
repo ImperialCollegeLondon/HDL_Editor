902af2010737e5b9f52944c2b664813bb4c3fff4
wilsonwang881 <mitfans881@gmail.com>
2019-04-07 15:51:53 +0100

    testing webpack's output management function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
-  let element = document.createElement('div');
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
   // add the image to the existing div
   var myIcon = new Image();
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
-
+  element.appendChild(btn);
+  
   return element;
 }
 
