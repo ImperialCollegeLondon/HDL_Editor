6995f211cd36ff081d6f3173a6a16123e8955ae0
wilsonwang881 <mitfans881@gmail.com>
2019-04-09 10:58:27 +0100

    continue testing on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+function resolve(filePath) {
+  return path.join(__dirname, filePath);
+}
+
+var babelOptions = fableUtils.resolveBabelOptions({
+  presets: [['es2015', { 'modules': false }]],
+  plugins: ['transform-runtime']
+});
+
+var isProduction = process.argv.indexOf('-p') >=0;
+console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
+
 module.exports = {
   mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
