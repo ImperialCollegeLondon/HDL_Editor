e2a740ae4f5d3f4f0f420f22524b8f31c1eb4b0d
wilsonwang881 <mitfans881@gmail.com>
2019-04-14 23:55:01 +0100

    working on Blocks.fs

--- src/Renderer/Blocks.fs
+++ src/Renderer/Blocks.fs
@@ -1,28 +1,62 @@
 ﻿(*
     Module: Blocks
-    Description: define different blocks and methods to interact with them
+    Description: define different blocks and methods to create and interact with them
 *)
 
 module Blocks
 
+/// define the dimensions of the canvas
+let canvasXMax = 10000;
+
+let canvasYMax = 10000;
+
+/// possible errors when creating or interacting with blocks
+type BlockError = 
+    | ``X coordinate is out of range`` of XCoordinate:int
+    | ``Y coordinate is out of range`` of YCoordinate:int
+
+/// built-in block types
 type BuiltinBlockTypes = 
-    | A
+    | BlockA
 
-type BuiltinBlockMethods = 
-    | Connect
-    | Disconnect
+/// pins on the blocks
+type BuiltinBlockPin = 
+    | PinA
 
-type BuiltinBlockConnectionStatus = 
-    | NotConnected
-    | Connected
+/// connection detials
+type ConnectionInfo = {
+    BusWidth: int;
+    SourceBlock: string;
+    SourceBlockType: BuiltinBlockTypes;
+    SourcePin: BuiltinBlockPin;
+    SinkBlock: string;
+    SinkBlockType: BuiltinBlockTypes;
+    SinkPin: BuiltinBlockPin
+}
 
-type BuildinBlockCoordinates = 
-    | XCoordinate of int
-    | YCoordinate of int
+/// the coordinates of the corners of the block
+type BuildinBlockCoordinates = {
+    TopLeftXCoordinate: int;
+    ButtomLeftYCoordinate: int;
+}
 
+/// information of a block on the canvas
 type BuiltinBlockInfo = {
     BlockType: BuiltinBlockTypes;
-    ConnectionStatus: BuiltinBlockConnectionStatus;
-    Coordinate: (B)
+    ConnectionStatus: ConnectionInfo List;
+    Coordinate: BuildinBlockCoordinates;
 }
+
+let checkBlockCenterWithinBorder (x:int) (y:int) = 
+    match x, y with
+    | a, _ when a > canvasXMax -> ``X coordinate is out of range`` (XCoordinate = a) |> Error
+    | _, b when b > canvasYMax -> ``Y coordinate is out of range`` (YCoordinate = b) |> Error
+    | c, d -> (c, d) |> Ok
+
+let createBuiltinBlock (x:int) (y:int) (blockType:BuiltinBlockTypes) =
+    match checkBlockCenterWithinBorder x y with
+    | Ok (c, d) -> Ok {BlockType = blockType;
+                       ConnectionStatus = [];
+                       Coordinate = {TopLeftXCoordinate = c; ButtomLeftYCoordinate = d}}
+    | Error e -> Error e
 
