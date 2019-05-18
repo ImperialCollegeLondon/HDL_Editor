df737f66aba9de50c6f7a1f91337e970beb10edc
wilsonwang881 <mitfans881@gmail.com>
2019-05-18 13:45:06 +0100

    hope to solve the problems in using the fable compiler

--- src/Renderer/Jointjs.fs
+++ src/Renderer/Jointjs.fs
@@ -97,6 +97,10 @@ let result() =
     rect?addTo(graph)
     *)
 
+    let canvas = document.getElementById("myCanvas")
+    let ctx = canvas?getContext("2d")
+    ctx?fillStyle <- "#FF0000"
+    ctx?fillRect <- (0,0,150,75)
     printfn "%O" attr
     console.log(attr)
 
