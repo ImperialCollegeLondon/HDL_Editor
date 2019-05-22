275a32cdb805844c320f19777d3439950fea26ec
wilsonwang881 <mitfans881@gmail.com>
2019-05-22 16:28:00 +0100

    added the setup file for developing under Windows

---
+++ setup.bat
@@ -0,0 +1,9 @@
+.paket\paket.exe install
+.paket\paket.exe update
+git submodule update --init --recursive
+git submodule update --recursive --remote
+dotnet nuget locals all --clear
+dotnet restore src\Main\Main.fsproj
+dotnet restore src\Renderer\Renderer.fsproj
+dotnet restore src\RendererAboutWindow\RendererAboutWindow.fsproj
+yarn install
