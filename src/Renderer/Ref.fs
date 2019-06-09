d274be79d0cc4ca867f0bf5616d3cc78edd5daa0
wilsonwang881 <mitfans881@gmail.com>
2019-06-09 22:49:53 +0100

    working on links. Need to implement the relational model and Verilog generating

--- src/Renderer/Ref.fs
+++ src/Renderer/Ref.fs
@@ -3,7 +3,23 @@
 *)
 module Ref
 
+open Fable.Core.JsInterop
+
 /// store the current active pane
 let mutable activePaneName:string = ""
 
-/// store the current
+/// store the current selected element
+/// for copying and cutting
+let mutable selectedElement:obj = createObj[]
+
+/// store the current graph model
+/// for file saving and loading
+let mutable currentGraphModel:obj = createObj[]
+
+/// store the current paper model
+/// for file saving and loading
+let mutable currentPaperModel:obj = createObj[]
+
+/// store the current relational mode
+/// for file saving and loading
+//let mutable currentRelationalModel:
