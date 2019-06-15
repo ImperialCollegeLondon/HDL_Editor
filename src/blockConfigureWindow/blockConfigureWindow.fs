e755eb852d1923cc65d3ff38c95ab8f93580a8ef
wilsonwang881 <mitfans881@gmail.com>
2019-06-16 00:07:29 +0100

    the program can generate Verilog code for blocks. Need to be able to generate Verilog code for the whole diagram

--- src/blockConfigureWindow/blockConfigureWindow.fs
+++ src/blockConfigureWindow/blockConfigureWindow.fs
@@ -353,7 +353,30 @@ let bindEventUpdateGUI () =
                                     content <- content + "  begin\n"
                                     content <- content + "    case({" + inputNames + "})\n"
 
-                                    content <- content + "    endcase"
+                                    let rec caseStatementInnerBeginEnd (row:int) (outputCount:int) (outputs:string array) (truthTable:string array) (res:string) = 
+                                        match outputCount with
+                                        | a when a < outputs.Length -> caseStatementInnerBeginEnd row (outputCount+1) outputs truthTable (res + outputs.[a] + " <= " + "1'b" + string truthTable.[row].[a] + ";\n      ")
+                                        | _ -> res
+
+                                    let rec lookupTableHorizontalLine (index:int) (truthTable:string array) (inputWidth:int) (res:string) = 
+                                        match index with
+                                        | a when a < truthTable.Length -> let lookupIn = "    " + (string inputWidth) + "'b" + (intToBinaryConverter a inputWidth) + ":\n"
+                                                                          let beginStatement = "      begin\n"
+                                                                          let innerStatements = caseStatementInnerBeginEnd a 0 outputIds truthTable "      "
+                                                                          let endStatement = "end\n"
+                                                                          lookupTableHorizontalLine (index+1) truthTable inputWidth (res + lookupIn + beginStatement + innerStatements + endStatement)
+
+                                        | _ -> res
+                                            
+                                    let defaultCaseTruthTable = 
+                                        let stringRes = [0..outputIds.Length-1]
+                                                        |> List.map (fun el -> "0")
+                                                        |> String.concat ""
+                                        [|stringRes|]
+
+                                    content <- lookupTableHorizontalLine 0 truthTable inputIds.Length content
+                                    content <- content + "    default: \n      begin\n" + caseStatementInnerBeginEnd 0 0 outputIds defaultCaseTruthTable "      " + "end\n"
+                                    content <- content + "    endcase\n"
                                     content <- content + "  end"
                                     content <- content + "\n" + "endmodule"
                                     content
